1c0316f151c5311224806bc57d6b4c65eb7186c1
Thiago Mandrik <thiagomandrik@gmail.com>
2018-10-03 00:01:43 -0300

    Adição das class de operadores lógicos, e de operadores de comparação.

---
+++ src/Operadores.java
@@ -0,0 +1,10 @@
+public class Operadores {
+    public static void main(String[] args){
+        int n1 = 10;
+        int n2 = 20;
+        int soma = n1+n2;
+        System.out.println("A soma eh: "+ (n1+n2));
+        //resto de divisão pode ser calculado usando %
+        System.out.println("O resto da divisao de 21/2 eh = "+ (21%2) +". Portanto 21 eh impar.");
+    }
+}
