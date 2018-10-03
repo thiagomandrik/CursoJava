925332cc85ad23d6263e773be3260b3cee483c51
Thiago Mandrik <thiagomandrik@gmail.com>
2018-10-02 22:05:42 -0300

    Adicionado uma class de estudo de variaveis primitivas, e um exercicio feito para fixar os conceitos aprendidos ate entao

---
+++ src/Exercicio1.java
@@ -0,0 +1,15 @@
+/*
+Crie um algoritmo que possua variaveis para armazenar os seguintes dados: nome, endereço e telefone
+Em seguida imprima da seguinte forma:
+<nome>, domiciliado no endereco <endereco> e telefone <telefone>, nao possui nenhum tipo de pendencia
+ */
+
+public class Exercicio1 {
+    public static void main(String[] args){
+        String nome = "Thiago Mandrik";
+        String endereco = "Rua Maximiliano de Francheski N° 43";
+        int telefone = 991597630;
+
+        System.out.println(nome+", domiciliado no endereco "+endereco+", e telefone "+telefone+", nao possui nenhum tipo de pendencia.");
+    }
+}
---
+++ src/ImprimindoVariaveis.java
@@ -0,0 +1,15 @@
+public class ImprimindoVariaveis {
+    public static void main(String[] args){
+        int idade = 10;
+        double salarioDouble = 3000;
+        float salarioFloat = 3000;
+        byte idadeByte = 12;
+        short idadeShort = 32767;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        long numeroGrange = 1000L;
+        char caractere = '\u0041'; //variaveis char armazenam 2 bytes
+        String nome = "Thiago";
+        System.out.println(caractere);
+    }
+}
