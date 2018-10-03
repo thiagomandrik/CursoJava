5c3d6d8579f16fd257c682c4d67fee9864d31aa2
Thiago Mandrik <thiagomandrik@gmail.com>
2018-10-03 19:57:39 -0300

    Class de operadores logicos &&(and) e ||(or).

---
+++ src/OperadoresLogicos.java
@@ -0,0 +1,10 @@
+public class OperadoresLogicos {
+    public static void main(String[] args){
+        int idade = 18;
+        float salario = 1000;
+
+        //&& = and e || = or
+        
+        System.out.println(idade >= 18 && salario >= 1000);
+    }
+}
