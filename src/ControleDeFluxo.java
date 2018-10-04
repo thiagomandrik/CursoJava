d52b5150a1bc3d9e7e3674307b6060d8a43f257f
Thiago Mandrik <thiagomandrik@gmail.com>
2018-10-03 22:26:19 -0300

    Class de controle de fluxo if, else, else if

--- src/ControleDeFluxo.java
+++ src/ControleDeFluxo.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class ControleDeFluxo {
+    public static void main(String[] args) {
+        //idade < 15 = infantil
+        //idade >= 15 && < 18 = juvenil
+        //idadee >= 18 adulto
+
+        int idade = 18;
+        String categoria;
+
+        if (idade < 15){
+            categoria = "Categoria Infantil.";
+        }else if (idade >= 15 && idade < 18) {
+            categoria = "Categoria Juvenil.";
+        }
+        else{
+            categoria = "Categoria Adulto.";
+        }
+        System.out.println(categoria);
+    }
 }
