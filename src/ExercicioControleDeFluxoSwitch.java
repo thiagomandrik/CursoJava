20796c4c42e776cf3ca8c489a6c1472ead8a4f59
Thiago Mandrik <thiagomandrik@gmail.com>
2018-10-09 20:18:26 -0300

    Modificação do Exercicio switch

--- src/ExercicioControleDeFluxoSwitch.java
+++ src/ExercicioControleDeFluxoSwitch.java
@@ -4,27 +4,17 @@ public class ExercicioControleDeFluxoSwitch {
     public static void main(String[] args) {
         int valor = 2;
 
-        switch (valor){
+        switch (valor) {
             case 1:
-                System.out.println("Domingo");
+            case 7:
+                System.out.println("Final de semana");
                 break;
             case 2:
-                System.out.println("Segunda-feira");
-                break;
             case 3:
-                System.out.println("Terça-feira");
-                break;
             case 4:
-                System.out.println("Quarta-feira");
-                break;
             case 5:
-                System.out.println("Quinta-feira");
-                break;
             case 6:
-                System.out.println("Sexta-feira");
-                break;
-            case 7:
-                System.out.println("Sábado");
+                System.out.println("Dia de semana");
                 break;
             default:
                 System.out.println("Número inválido");
