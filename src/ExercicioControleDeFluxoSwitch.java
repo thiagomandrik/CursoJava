a174bfca96dcb8c76714ca740508313b55e508e2
Thiago Mandrik <thiagomandrik@gmail.com>
2018-10-08 21:23:08 -0300

    Class de switch e exercicio usando switch

---
+++ src/ExercicioControleDeFluxoSwitch.java
@@ -0,0 +1,34 @@
+import javax.xml.bind.SchemaOutputResolver;
+
+public class ExercicioControleDeFluxoSwitch {
+    public static void main(String[] args) {
+        int valor = 2;
+
+        switch (valor){
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda-feira");
+                break;
+            case 3:
+                System.out.println("Terça-feira");
+                break;
+            case 4:
+                System.out.println("Quarta-feira");
+                break;
+            case 5:
+                System.out.println("Quinta-feira");
+                break;
+            case 6:
+                System.out.println("Sexta-feira");
+                break;
+            case 7:
+                System.out.println("Sábado");
+                break;
+            default:
+                System.out.println("Número inválido");
+                break;
+        }
+    }
+}
